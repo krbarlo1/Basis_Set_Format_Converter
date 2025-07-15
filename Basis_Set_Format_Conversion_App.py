8c7e19ed2eb9f02cf7272245b8d05d397f330e80
krbarlo1 <50717494+krbarlo1@users.noreply.github.com>
2025-07-15 13:36:23 -0500

    Update Basis_Set_Format_Conversion_App.py

--- Basis_Set_Format_Conversion_App.py
+++ Basis_Set_Format_Conversion_App.py
@@ -1,82 +1,133 @@
 import streamlit as st
 import basis_set_exchange as bse
 import os
+
 # Set page config
-st.set_page_config(page_title='Basis Set Converter', layout='wide', page_icon="🧊",
-menu_items={
-         'About': "# This online tool allows you to enter a basis set (and ECPs) in the form of text input for a variety of Quantum Chemistry softwares, and convert it to another format."
-     })
+st.set_page_config(
+    page_title='Basis Set Converter',
+    layout='wide',
+    page_icon="🧊",
+    menu_items={
+        'About': "# This online tool allows you to enter a basis set (and ECPs) in the form of text input for a variety of Quantum Chemistry softwares, and convert it to another format."
+    }
+)
 
 # Sidebar stuff
 st.sidebar.write('# About')
-st.sidebar.write('### Made By [Manas Sharma](http://manas.bragitoff.com/)')
-st.sidebar.write('### in the group of [Prof. Dr. Marek Sierka](https://www.cmsg.uni-jena.de/)')
-st.sidebar.write('### for the [TURBOMOLE](https://www.turbomole.org/) program package')
 st.sidebar.write('### *Powered by [Basis Set Exchange](https://github.com/MolSSI-BSE/basis_set_exchange)*')
 st.sidebar.write('Basis Set Exchange (BSE) is a repository for quantum chemistry basis sets, which also provides a flexible and powerful API to facilitate reading/writing or converting basis sets.')
 st.sidebar.write('[API Documentation for BSE](https://molssi-bse.github.io/basis_set_exchange/index.html)')
 st.sidebar.write('The BSE library is available under the [BSD 3-Clause license](https://github.com/MolSSI-BSE/basis_set_exchange/blob/master/LICENSE)')
-st.sidebar.write("For some reason, the master branch of BSE on GitHub doesn't have readers/writers available for the `crystal` basis set format. ")
-st.sidebar.write('Fortunately, there is a branch by [Susi Lehtola](https://github.com/susilehtola) available [here](https://github.com/susilehtola/basis_set_exchange/tree/crystal) which provides support for `crystal` format.')
-st.sidebar.write('This online tool uses that particular branch.')
 
 # Main app
 st.write('# Basis Set Format Converter')
 st.write('This online tool allows you to enter a basis set in the form of text input for a variety of Quantum Chemistry softwares, and convert it to another format.')
 
+placeholder_cfour_basis = '''# Example Cfour basis set for Hydrogen
 
-placeholder_basis_str = '''# Example input: def2-SVP basis for Carbon in Turbomole format
-$basis
-*
-c def2-SVP
-*
-   5  s
-  1238.4016938      .54568832082E-02
-  186.29004992      .40638409211E-01
-  42.251176346      .18025593888
-  11.676557932      .46315121755
-  3.5930506482      .44087173314
-   1  s
-  .40245147363      1.0000000000
-   1  s
-  .13090182668      1.0000000000
-   3  p
-  9.4680970621      .38387871728E-01
-  2.0103545142      .21117025112
-  .54771004707      .51328172114
-   1  p
-  .15268613795      1.0000000000
-   1  d
-  .80   1.00
-*
-# K. Eichkorn, F. Weigend, O. Treutler, R. Ahlrichs; Theor. Chem. Acc. 97, 119 (1997).
-$end
-'''
+H:aug-cc-pV5Z-DK
+V5ZPD   All-electron Douglas-Kroll Valence Quintuple Zeta + Polarization
+
+  5
+    0    1    2    3    4
+    6    5    4    3    2
+    9    5    4    3    2
+
+402.0000000 60.2400000 13.7300000 3.9050000 1.2830000 
+0.4655000 0.1811000 0.0727900 0.0207000 
+
+0.0000000 0.0000000 0.0002836 0.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.0021692 0.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.0112082 0.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.0448854 0.0000000 0.0000000 0.00000000 
+1.0000000 0.0000000 0.1423120 0.0000000 0.0000000 0.00000000 
+0.0000000 1.0000000 0.3309635 0.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.4362734 1.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.1764335 0.0000000 1.0000000 0.00000000 
+0.00000000 0.00000000 0.00000000 0.00000000 0.00000000 1.0000000 
+
+4.5160000 1.7120000 0.6490000 0.2460000 0.0744000 
+
+1.0000000 0.0000000 0.0000000 0.0000000 0.00000000 
+0.0000000 1.0000000 0.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 1.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 0.0000000 1.0000000 0.00000000 
+0.00000000 0.00000000 0.00000000 0.00000000 1.0000000 
+
+2.9500000 1.2060000 0.4930000 0.1560000 
+
+1.0000000 0.0000000 0.0000000 0.00000000 
+0.0000000 1.0000000 0.0000000 0.00000000 
+0.0000000 0.0000000 1.0000000 0.00000000 
+0.00000000 0.00000000 0.00000000 1.0000000 
+
+2.5060000 0.8750000 0.2740000 
+
+1.0000000 0.0000000 0.00000000 
+0.0000000 1.0000000 0.00000000 
+0.00000000 0.00000000 1.0000000 
+
+2.3580000 0.5430000 
+
+1.0000000 0.00000000 
+0.00000000 1.0000000 
+
+
 col1, col2 = st.columns(2)
 col1.write('## INPUT')
-input_format = col1.selectbox('Select the input basis set format',
-     ( 'turbomole', 'crystal', 'gaussian94', 'nwchem', 'dalton','molcas','cfour','genbas','gbasis','demon2k','ricdlib', 'cp2k'))
-input_basis_str = col1.text_area(label='Enter your own Basis Set here', value = placeholder_basis_str, placeholder = 'Put your text here', height=400)
+input_format = col1.selectbox(
+    'Select the input basis set format',
+    (
+        'cfour',
+         'molpro'
+    ),
+    index=0  # Default to cfour
+)
+input_basis_str = col1.text_area(
+    label='Enter your own Basis Set here',
+    value=placeholder_cfour_basis,
+    placeholder='Put your Cfour-formatted basis set here',
+    height=400
+)
 # Get rid of empty lines
-input_basis_str = os.linesep.join([s for s in input_basis_str.splitlines() if s])
-
-
-
+input_basis_str = os.linesep.join([s for s in input_basis_str.splitlines() if s.strip()])
 
 col2.write('## OUTPUT')
-output_format = col2.selectbox('Select the output basis set format',
-     ( 'crystal', 'turbomole', 'gaussian94', 'nwchem', 'dalton','molcas','cfour','genbas','gbasis','demon2k','ricdlib','psi4','qchem','orca','cp2k','gamess_us','gamess_uk','pqs','molpro','cfour','xtron','acesii'))
-# Parse the processed list of basis set lines (use .splitlines)
-# basis_read_from_crystal_format = bse.readers.crystal.read_crystal(input_basis_str.splitlines())
-# BSE JSON Format
-basis_dict_bse = bse.readers.read.read_formatted_basis_str(input_basis_str, basis_fmt=input_format)
-output_basis_str = bse.writers.write.write_formatted_basis_str(basis_dict_bse, fmt=output_format)
-col2.text_area(label='Converted basis set in the format selected by you',value=output_basis_str, height=400)
+output_format = col2.selectbox(
+    'Select the output basis set format',
+    (
+        'molpro',
+        'cfour'
+    ),
+    index=0  # Default to molpro
+)
 
-st.write('## YouTube Tutorial')
-_, container, _ = st.columns([100, 300, 100])
-container.video('https://www.youtube.com/watch?v=C_9zUPz32X8')
+# Only try to convert if there is some input
+output_basis_str = ""
+basis_dict_bse = None
+error_message = ""
+if input_basis_str.strip():
+    try:
+        # Parse and convert
+        basis_dict_bse = bse.readers.read.read_formatted_basis_str(
+            input_basis_str, basis_fmt=input_format
+        )
+        output_basis_str = bse.writers.write.write_formatted_basis_str(
+            basis_dict_bse, fmt=output_format
+        )
+    except Exception as e:
+        error_message = f"Error during conversion: {e}"
+
+col2.text_area(
+    label='Converted basis set in the format selected by you',
+    value=(output_basis_str if not error_message else error_message),
+    height=400
+)
+
 
 st.write('## Basis Set Exchange JSON Format')
 st.write('For debugging')
-st.write(basis_dict_bse)
+if basis_dict_bse:
+    st.write(basis_dict_bse)
+elif error_message:
+    st.write(error_message)
